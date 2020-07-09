3a4020d6d38aef97205fade8e3f1559a7224835d
Carlos David Moreno Raigoza <camoreno@personalsoft.com.co>
2020-07-08 21:54:21 -0500

    Avance Login

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,25 @@
-let btnIngresar = document.getElementById("btnIngresar");
+let btnLogin = document.getElementById("btnIngresar");
 let loguin = document.querySelector(".loguin");
 let fondoOscuro = document.querySelector(".fondoOscuro");
-let cerrarVentana = document.querySelector(".cerrarVentana");
+let btnCerrar = document.querySelector(".cerrarVentana");
+
+const activarLogin=(e)=>{
+    e.preventDefault();
+    
+    setTimeout(() =>{
+        fondoOscuro.style.display="block";
+    }, 200);
+
+    setTimeout(()=>{
+        loguin.style.display="block";
+    }, 300);
+}
+
+const cerrarLogin=()=>{
+    loguin.style.display="none";
+    fondoOscuro.style.display="none";
+}
+
+
+btnLogin.addEventListener("click",activarLogin);
+btnCerrar.addEventListener("click", cerrarLogin);
