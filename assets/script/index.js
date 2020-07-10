400a5aca87d73418994d98675095cf33a7de08c8
Carlos David Moreno Raigoza <camoreno@personalsoft.com.co>
2020-07-09 21:34:10 -0500

    Loguin Final

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,46 +1,83 @@
-let btnLogin = document.getElementById("btnIngresar");
+let btnIngresar = document.getElementById("btnIngresar");
 let loguin = document.querySelector(".contenedor");
 let fondoOscuro = document.querySelector(".fondoOscuro");
 let inputs = document.querySelectorAll(".input");
+let btnLogin = document.querySelector(".btnLogin");
+let user = document.getElementById("user");
+let pass = document.getElementById("pass");
+let btnForgot = document.getElementById("Forgot");
+
 /* let btnCerrar = document.querySelector(".cerrarVentana"); */
 
 //ACCIONES DEL LOGIN
-function focusFun(){
+function focusFun() {
     let parent = this.parentNode.parentNode;
     parent.classList.add("focus");
 }
 
-function blurFun(){
-    let parent=this.parentNode.parentNode;
-    if(this.value==""){
+function blurFun() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
         parent.classList.remove("focus");
     }
 }
-inputs.forEach(input =>{
-    input.addEventListener("focus",focusFun);
-    input.addEventListener("blur",blurFun);
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFun);
+    input.addEventListener("blur", blurFun);
 });
 
 
-
 //ACTIVAMOS EL LOGIN POR EL BOTON INGRESAR
-const activarLogin=(e)=>{
+const activarLogin = (e) => {
     e.preventDefault();
-    
-    setTimeout(() =>{
-        fondoOscuro.style.display="block";
+
+    setTimeout(() => {
+        fondoOscuro.style.display = "grid";
     }, 200);
 
-    setTimeout(()=>{
-        loguin.style.display="block";
+    setTimeout(() => {
+        loguin.style.display = "grid";
     }, 300);
 }
 
-/* const cerrarLogin=()=>{
-    loguin.style.display="none";
-    fondoOscuro.style.display="none";
-} */
+function ingresar() {
+    if (user.value == "" || pass.value == "") {
+        swal.fire({
+            title: "Debes llenar todos los campos",
+            icon: "error"
+        })
+    } else {
+        swal.fire({
+            title: "Bienvenido a VipTravel",
+            text: "señor@" + " " + user.value,
+            icon: "success"
+        })
+        loguin.style.display = "none";
+        fondoOscuro.style.display = "none";
+        user.value = "";
+        pass.value = "";
+    }
+}
+
+function olvidarUser() {
+    swal.fire({
+        title: "¿Olvidaste tu usuario o contraseña?",
+        text: "Ingresa tu email y te enviaremos los datos",
+        icon: "question",
+        input: "text",
+        inputPlaceholder: "Email"
+    })
+
+    /*     if(input.value !==""){
+            swal.fire({
+                title: "Información enviada",
+                text: "Por favor revisa tu correo",
+                icon: "success"
+            })
+        } */
+}
 
 
-btnLogin.addEventListener("click",activarLogin);
-/* btnCerrar.addEventListener("click", cerrarLogin); */
+btnIngresar.addEventListener("click", activarLogin);
+btnLogin.addEventListener("click", ingresar);
+btnForgot.addEventListener("click", olvidarUser);
